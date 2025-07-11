832314ff62f715544741b9ef17e13f9c094674ad
aryam <maran0@purdue.edu>
2025-07-11 11:56:49 -0400

    completed code for first url to scrape and dump json article in /raw/

---
+++ src/data_collection/news_scraper.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import os,re,time
+
+output_dir = "../../data/raw"
+
+url_1 = "https://insideclimatenews.org/post-sitemap.xml"
+resp_1 = requests.get(url_1)
+soup_1 = BeautifulSoup(resp_1.content, "xml")
+
+article_urls_1 = [loc.text for loc in soup_1.find_all("loc")]
+
+def clean_filenames(title: str) -> str:
+  filename = re.sub(r'[<>:"/\\|?*]', "_", title)
+  return filename[:100].strip()
+
+def scrape_article(url):
+  try:
+    res = requests.get(url, timeout=10)
+    res.raise_for_status
+    soup = BeautifulSoup(res.content, "html-parser")
+
+    title = soup.find("h1").text.strip() if soup.find("h1") else "untitled"
+    date_tag = soup.find("time")
+    date = date_tag.get("datetime") if date_tag and date_tag.has_attr("datetime") else None
+    paragraphs = soup.find_all("p")
+    content = "\n".join([p.text.strip() for p in paragraphs if p.text.strip()])
+
+    return {
+      "url": url,
+      "title": title,
+      "date": date,
+      "content": content
+    }
+  except Exception as e:
+    print(f"Error scraping {url}: {e}")
+    return None
+
+for i, url in enumerate(article_urls_1):
+  print(f"[{i+1}/{len(article_urls_1)}] Downloading: {url}")
+  article_data = scrape_article(url)
+  if article_data:
+    filename = f"{i+1:04d}_{clean_filenames(article_data['title']).json}"
+    file_path = os.path.join(output_dir, filename)
+
+    if os.path.exists(file_path):
+      print(f"Skipping existing file: {filename}")
+      continue
+
+    with open(file_path, "w", encoding="utf-8") as f:
+      json.dump(article_data, f, indent=2, ensure_ascii=False)
+
+  time.sleep(1)
