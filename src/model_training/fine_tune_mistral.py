8b09771b64752b1d15699de67d18e9c50ed9700d
aryam <maran0@purdue.edu>
2025-08-09 17:37:12 -0400

    added drive checkpointing

--- src/model_training/fine_tune_mistral.py
+++ src/model_training/fine_tune_mistral.py
@@ -81,7 +81,7 @@ trainer = Trainer(
   tokenizer=tokenizer
 )
 
-trainer.train()
+trainer.train(resume_from_checkpoint="/content/drive/MyDrive/mistral-qlora/checkpoint-<step>")
 
 print(f"Saving QLORA weights to {OUTPUT_DIR}")
 model.save_pretrained(OUTPUT_DIR)
