363bda869fe7c8ffc889b2d16d754e4be4d1e81d
aryam <maran0@purdue.edu>
2025-08-09 21:36:24 -0400

    resume from checkpoint-1600

--- src/model_training/fine_tune_mistral.py
+++ src/model_training/fine_tune_mistral.py
@@ -81,7 +81,7 @@ trainer = Trainer(
   tokenizer=tokenizer
 )
 
-trainer.train()
+trainer.train(resume_from_checkpoint="/content/drive/MyDrive/mistral-qlora/checkpoint-1600")
 
 print(f"Saving QLORA weights to {OUTPUT_DIR}")
 model.save_pretrained(OUTPUT_DIR)
